07d96db47c4ab837d54c28aa49b20e4eba5c1105
Hector1241 <hectorjr.acosta01@gmail.com>
2025-03-22 17:59:20 -0400

    Update main.c

--- Tarea 9/Investigacion sobre las listas enlazadas y doblemente enlazadas en lenguaje C/5 ejemplos de uso con código/Ejemplo 2/main.c
+++ Tarea 9/Investigacion sobre las listas enlazadas y doblemente enlazadas en lenguaje C/5 ejemplos de uso con código/Ejemplo 2/main.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    printf("Hello world!\n");
+struct Nodo {
+    int dato;
+    struct Nodo* siguiente;
+};
+
+void imprimirLista(struct Nodo* cabeza) {
+    while (cabeza != NULL) {
+        printf("%d -> ", cabeza->dato);
+        cabeza = cabeza->siguiente;
+    }
+    printf("NULL\n");
+}
+
+int main() {
+    struct Nodo* cabeza = NULL;
+    struct Nodo* segundo = NULL;
+    struct Nodo* tercero = NULL;
+
+    cabeza = (struct Nodo*)malloc(sizeof(struct Nodo));
+    segundo = (struct Nodo*)malloc(sizeof(struct Nodo));
+    tercero = (struct Nodo*)malloc(sizeof(struct Nodo));
+
+    cabeza->dato = 1;
+    cabeza->siguiente = segundo;
+
+    segundo->dato = 2;
+    segundo->siguiente = tercero;
+
+    tercero->dato = 3;
+    tercero->siguiente = NULL;
+
+    imprimirLista(cabeza);
+
+    free(cabeza);
+    free(segundo);
+    free(tercero);
+
     return 0;
 }
